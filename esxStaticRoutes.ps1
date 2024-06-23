a6e44b73f9108876889affa06409db97130a1276
John Sokol <soksy@soksy.org>
2024-06-23 12:16:11 +1000

    deleted something or other

--- esxStaticRoutes.ps1
+++
@@ -1,9 +0,0 @@
-Set-PowerCLIConfiguration -Scope User -ParticipateInCEIP $false -Confirm:$false
-Set-PowerCLIConfiguration -InvalidCertificateAction Ignore -Confirm:$false
-$IPLIST = '192.168.2.30', '192.168.2.31', '192.168.2.32', '192.168.2.35', '192.168.2.36', '192.168.2.37'
-
-Foreach ($IP in $IPLIST) {
-	Connect-VIServer -Server $IP -Protocol https -User root -Password 'XXXXXXXXXXXX'
-	New-VMHostRoute -Confirm:$false -Destination 192.168.5.0 -Gateway 192.168.2.254 -PrefixLength 24 -VMHost $IP 
-}
-
